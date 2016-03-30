57328eec903d1979d258a93bac08d4a75dff7070
dbrandwein <david.brandwein@gmail.com>
2016-03-30 09:05:35 -0400

    fixing nginx module again

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -25,6 +25,16 @@ class nginx {
     mode => '0755',
   }
   
+  file { '/etc/nginx/nginx.conf' :
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+}
+
   file { '/var/nginx/conf.d/default.conf' :
     ensure => file,
     owner => 'root',
