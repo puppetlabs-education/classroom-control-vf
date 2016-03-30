735bb6fdbac38de8b742dc4fc798a42c89dade21
dbrandwein <david.brandwein@gmail.com>
2016-03-30 06:42:24 -0400

    Adding host entry
    testing.puppetlabs.vm

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,6 +49,11 @@ node default {
     path    => "/usr/local/bin"
   }
   
+  host { "testing.puppetlabs.vm" :
+    ensure => present,
+    ip => 127.0.0.1,
+  }
+  
   # file { 'motd' :
   #   ensure  => present,
   #   owner   => root,
