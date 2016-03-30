e0b9c35b836dd9822be1c38294b2ada8fe52a601
dbrandwein <david.brandwein@gmail.com>
2016-03-30 07:10:07 -0400

    adding new module reference

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  
+  include users
+  
   notify { "Hello, my name is ${::hostname}": }
   
   exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
