0082f380225ad9cef917578c6f9409fbba22c374
dbrandwein <david.brandwein@gmail.com>
2016-03-30 08:04:26 -0400

    more troubleshooting skeleton

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,9 @@
 class skeleton {
   file { '/etc/skel' :
     ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
   }
   file { skel-bashrc :
     ensure => present,
