d11fdee29bf23a89c83e7c57af225fc33a4ebd26
dbrandwein <david.brandwein@gmail.com>
2016-03-30 07:57:33 -0400

    adding a few fields to skel-bashrc

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -4,6 +4,9 @@ class skeleton {
   }
   file { skel-bashrc :
     ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
     source => "puppet:///modules/skeleton/skel-bashrc",
     path => '/etc/skel/.bash_rc',
   }
