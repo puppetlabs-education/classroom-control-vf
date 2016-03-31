184e47f8d1cab793f945a626f257cc4531ff0134
dbrandwein <david.brandwein@gmail.com>
2016-03-31 11:33:32 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,43 +1,14 @@
 class nginx (
-  $root = undef,
-) {
-  case $::osfamily {
-    'redhat': {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $blockdir = '/etc/nginx/conf.d'
-      $logdir = '/var/log/nginx'
-      $service = 'nginx'
-      $user = 'nginx'
-    }
-    'debian': {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $blockdir = '/etc/nginx/conf.d'
-      $logdir = '/var/log/nginx'
-      $service = 'nginx'
-      $user = 'www-data'
-    }
-    'windows': {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $blockdir = 'C:/ProgramData/nginx/conf.d'
-      $logdir = 'C:/ProgramData/nginx/logs'
-      $service = 'nginx'
-      $user = 'nobody'
-    }
-    default: { fail("Sorry, your platform '${::osfamily}' is not supported.") }
-  }
-  $real_docroot = pick($root, $docroot)
+    $package = $nginx::params::package,
+    $owner = $nginx::params::owner,
+    $group = $nginx::params::group,
+    $docroot = $nginx::params::docroot,
+    $confdir = $nginx::params::confdir,
+    $blockdir = $nginx::params::blockdir,
+    $logdir = $nginx::params::logdir,
+    $service = $nginx::params::service,
+    $user = $nginx::params::user,
+) inherits nginx::params {
   File {
     ensure => file,
     owner => $owner,
@@ -61,10 +32,10 @@ class nginx (
     enable => true,
     subscribe => [ File['nginx main config'], File['nginx default block'] ],
   }
-  file { $real_docroot:
+  file { $docroot:
     ensure => directory,
   }
-  file { "${real_docroot}/index.html":
+  file { "${docroot}/index.html":
     source => 'puppet:///modules/nginx/index.html',
   }
 }
