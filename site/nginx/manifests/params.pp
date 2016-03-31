a127baaa542833cf6f8879d6caef23858ef4775b
dbrandwein <david.brandwein@gmail.com>
2016-03-31 11:30:55 -0400

    Create params.pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,38 @@
+class nginx::params {
+  case $::osfamily {
+    'redhat': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+      $service = 'nginx'
+      $user = 'nginx'
+    }
+    'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+      $service = 'nginx'
+      $user = 'www-data'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir = 'C:/ProgramData/nginx/logs'
+      $service = 'nginx'
+      $user = 'nobody'
+    }
+    default: { fail("Sorry, your platform '${::osfamily}' is not supported.") }
+  }
+}
