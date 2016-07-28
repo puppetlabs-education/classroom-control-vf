0d6b7fc8a9ba2703113e263ce8c55b9d865792f9
rkancha <ramu.kancha@massmail.state.ma.US>
2016-07-28 12:25:50 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,11 +1,14 @@
 class users::admins {
 users::managed_user { 'joe': }
+users::managed_user { 'chen': }
 users::managed_user { 'alice':
-group => 'staff',
+#group => 'staff',
+group => 'admin',
 }
 users::managed_user { 'aaron':
 group => 'staff',
 }
+#group { 'staff':
 group { 'staff':
 ensure => present,
 }
