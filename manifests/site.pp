6cd8aec34fdec38dca6bfd3c722964a48b7b7d01
rkancha <ramu.kancha@massmail.state.ma.US>
2016-07-29 09:21:57 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,13 +38,18 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+#node default {
 # This is where you can declare classes for all nodes.
 # Example:
 # class { 'my_class': }
-$message = hiera('message')
-notify { $message: }
-}
+#$message = hiera('message')
+#notify { $message: }
+#}
+
+#node default {
+#notify {"Hello my Name is  ${::fqdn}":}
+#}
+
 
 #node default {
   # This is where you can declare classes for all nodes.
@@ -54,11 +59,11 @@ notify { $message: }
 #   svirt_cap =capitalize($::virtual)
 #   notify { " This host is a virtual S{virt_cap} host.\n": }
 
-#node default {
+node default {
 # This is where you can declare classes for all nodes.
 # Example:
-# class { 'my_class': }
-#notify { "Hello, my name is ${::hostname}": }
+ class { 'my_class': }
+notify { "Hello, my name is ${::hostname}": }
 
 #file { '/etc/motd':
 #ensure => file,
@@ -93,6 +98,3 @@ ip => '127.0.0.1',
 include users
 include skeleton
 
-#node default {
-#notify {"Hello my Name is  ${::fqdn}":}
-#}
