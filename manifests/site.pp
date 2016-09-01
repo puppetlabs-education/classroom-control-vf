e45d43110b2c294d73cef74aeae5d2ea3713e119
aestolan <analyn.estolano@macquarie.com>
2016-09-01 22:40:29 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,10 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
+  if $::virtual != 'physical' {
+  $vmname = capitalize($::virtual)
+  notify { "This is a ${vmname} virtual machine.": }
+}
   
  # exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
 #  path => '/usr/bin:/usr/local/bin',
