c623f27e1ad367406d0954075fcfa4e519542077
jimhoviolin <jimho_violin@hotmail.com>
2016-12-01 13:35:35 -0500

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,10 @@
+define users::managed_user {
+   user { $title:
+      ensure => present,
+   }
+   file { "/home/${title}":
+      ensure => directory,
+      owner => $title,
+      group => $title,
+   }
+}
