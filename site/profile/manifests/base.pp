76b580c79fdb67e87217e3ec8e2b15e5965e03ad
jimhoviolin <jimho_violin@hotmail.com>
2016-12-02 10:23:10 -0500

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,6 @@
 class profile::base {
 
-  #$message = hiera('message', 'no message for you!')
-  $message = hiera('message')
+  $message = hiera('message', 'no message for you!')
   
   notify { $message: }
 }
