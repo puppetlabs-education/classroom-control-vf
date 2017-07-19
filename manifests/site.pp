198c489bed180c9dc1f1941f4d7d5d3c4beb7734
Jeff <jeffr@doit.tamu.edu>
2017-07-19 09:02:01 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -41,7 +41,8 @@ ini_setting { 'random ordering':
 node default {
   include role::classroom
   }
-exec { 'motd'
+
+exec { 'motd':
     command => "cowsay 'Welcome to $fqdn' > /etc/motd",
     path    => '/usr/local/bin',
     creates => '/etc/motd',
