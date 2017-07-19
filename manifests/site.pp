598a279ccbab7be3c529da68eb9e22aacf112cf9
Jeff <jeffr@doit.tamu.edu>
2017-07-19 09:22:43 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,37 +1,36 @@
 ## site.pp ##
 
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
-
-## Active Configurations ##
-
-# Disable filebucket by default for all File resources:
 File { backup => false }
 
 # Randomize enforcement order to help understand relationships
-#ini_setting { 'random ordering':
-#  ensure  => present,
-#  path    => "${settings::confdir}/puppet.conf",
-#  section => 'agent',
-#  setting => 'ordering',
-#  value   => 'title-hash',
+ini_setting { 'random ordering':
+  ensure  => present,
+  path    => "${settings::confdir}/puppet.conf",
+  section => 'agent',
+  setting => 'ordering',
+  value   => 'title-hash',
+}
+
+#node josephoaks.puppetlabs.vm {
+#  class { 'userprefs::vim':
+#    line_number => false,
+#  }
 #}
 
 node default {
   include role::classroom
-
-file { '/etc/motd':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  content => "Today I learned what it means to manage state using Puppet.\n",
+  
+  #file { 'motd':
+  #  ensure  => file,
+  #  path    => '/etc/motd',
+  #  owner   => 'root',
+  #  group   => 'root',
+  #  content => "Isn't Puppet fun!\n",
+  #}
+  
+  exec { 'motd':
+    command => "cowsay 'Welcome to $fqdn!' > /etc/motd",
+    path    => '/usr/local/bin',
+    creates => '/etc/motd',
   }
 }
-
