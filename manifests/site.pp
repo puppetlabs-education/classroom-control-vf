0312f34643e69af650bb05961e89d15b3cefad5d
Jeff <jeffr@doit.tamu.edu>
2017-07-19 08:55:06 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,8 +39,11 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
   include role::classroom
+
+ exec { 'motd'
+    command => "cowsay 'Welcome to $fqdn' > /etc/motd",
+    path    => '/usr/local/bin',
+    creates => '/etc/motd',
+  }
 }
