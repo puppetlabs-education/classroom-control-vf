41c17ed2da6f240a70248bf49aa63ea1c97adb35
Jeff <jeffr@doit.tamu.edu>
2017-07-19 09:21:14 -0500

    Update init.pp

--- site/skeleton/manifest/init.pp
+++ site/skeleton/manifest/init.pp
@@ -1,1 +1,18 @@
-
+class skeleton {
+  file { 'skel':
+    ensure => directory,
+    path   => '/etc/skel',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  
+  file { 'bashrc':
+    ensure => file,
+    path   => '/etc/skel/.bashrc',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
+  }
+}
